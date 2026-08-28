389173dbe0d565d932b436e2f00fff9143948bda
RACK-2026 <2796690649@qq.com>
2026-08-28 17:24:46 +0800

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    proxy: {
+      '/api-proxy': {
+        target: 'http://192.168.130.125:6363',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api-proxy/, ''),
+      },
+    },
+  },
+})
+
